4a0a7e40adb11983839412352fabdce2fa456da2
albarracin_ale <albarracin.alejandro12@gmail.com>
2023-01-26 18:27:36 -0300

    primer commit

--- src/componentes/Productos/ProductoItem.js
+++ src/componentes/Productos/ProductoItem.js
@@ -15,11 +15,11 @@ export const ProductoItem = ({
 
   return (
     <div className="producto">
-      <a href="!#">
+      <Link to={`/producto/${id}`}>
         <div className="producto__img">
           <img src={image} alt={title} />
         </div>
-      </a>
+      </Link>
       <div className="producto__footer">
         <h1>{title} </h1>
         <p>{category}</p>
@@ -30,9 +30,9 @@ export const ProductoItem = ({
           Añadir al carrito
         </button>
         <div>
-          <a href="!#" className="btn">
+          <Link to={`/producto/${id}`} className="btn">
             Vista
-          </a>
+          </Link>
         </div>
       </div>
     </div>
